aaa01fd957c4bdb4c39b40d630110f0468486615
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2021-04-06 13:29:44 +0200

    Oprettelse af DTO'er, forbindelse til test API, indsættelse af biometric-billede og tilbagerulning af Nugets så Flyout-menuen virker igen

---
+++ MoleWatchApp/Models/APICommunication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataClasses.DTO;
+using APIWebServiesConnector;
+
+
+namespace MoleWatchApp.Models
+{
+    public class APICommunication
+    {
+        private PatientInfoDTO newPatientInfoDto;
+        private IAPIService API;
+
+        public APICommunication()
+        {
+            API = new StubApiService();
+
+        }
+
+        public bool VerifyPasswordWithAPI(string Username, string Password)
+        {
+            LoginInfoDTO NewLogin = new LoginInfoDTO();
+
+            NewLogin.Password = Password;
+            NewLogin.Username = Username;
+
+
+            try
+            {
+                newPatientInfoDto = API.GetObject<PatientInfoDTO, LoginInfoDTO>("PostLoginPatient", NewLogin);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            if (newPatientInfoDto != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- MoleWatchApp/Models/LoginModel.cs
+++ MoleWatchApp/Models/LoginModel.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 
+using DataClasses.DTO;
+
 namespace MoleWatchApp.Models
 {
     public class LoginModel
     {
+        private APICommunication APIConnection;
+
+        public LoginModel()
+        {
+            APIConnection = new APICommunication();
+
+        }
+
         public bool VerifyPassword(string Username, string Password)
         {
             // TODO indskriv kommunikationen med API-klassen her.
-            return false;
+
+
+            bool LoginVerified = APIConnection.VerifyPasswordWithAPI(Username, Password);
+
+            return LoginVerified;
+
         }
     }
 
