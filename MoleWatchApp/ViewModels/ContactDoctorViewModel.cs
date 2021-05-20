be53db66c18ea9fb117a04602d7c71eed5e38a88
Signe Gram Sand <signegramsand@gmail.com>
2021-05-20 11:49:14 +0200

    Implemenerting af view for contact doctor

---
+++ MoleWatchApp/ViewModels/ContactDoctorViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoleWatchApp.ViewModels
+{
+    public class ContactDoctorViewModel : BaseViewModel
+    {
+        private string doctorName;
+        private string doctorAdress;
+
+        public string DoctorName
+        {
+            get
+            {
+                return doctorName;
+            }
+            set
+            {
+                doctorName = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string DoctorAdress
+        {
+            get
+            {
+                return doctorAdress;
+            }
+            set
+            {
+                doctorAdress = value;
+                this.OnPropertyChanged();
+            }
+        }
+    }
+}
